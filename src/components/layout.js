e35c5b3f93656fa4e6ef8e666acfb8346411e484
Josh Sauder <joshsauder@gmail.com>
2020-01-04 16:42:57 -0500

    added pricing

--- src/components/layout.js
+++ src/components/layout.js
@@ -347,63 +347,53 @@ function Layout({ children }) {
               
               <div className="container mx-auto flex flex-wrap pt-4 pb-12">
               
-                <h3 className="w-full my-2 text-3xl font-bold leading-tight text-center text-gray-800">Text Cards</h3>
+                <h3 className="w-full my-2 text-3xl font-bold leading-tight text-center text-gray-800">Updates</h3>
                 <div className="w-full mb-4">	
                   <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
                 </div>
               
-                <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+                <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink mb-4 mt-4">
                   <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
                     <a href="https://www.gatsbyjs.org/" className="flex flex-wrap no-underline hover:no-underline">
-                      <p className="w-full text-gray-600 text-xs md:text-sm px-6">Blazing Fast</p>
-                      <div className="w-full font-bold text-xl text-gray-800 px-6">Static Progressive Web App</div>
+                      <p className="w-full text-gray-600 text-xs md:text-sm px-6">iOS 13 Supprt</p>
+                      <div className="w-full font-bold text-xl text-gray-800 px-6">Version 2.1</div>
                       <p className="text-gray-800 text-base px-6 mb-5">
-                        Why Gatsby? Gatsby is a static PWA (Progressive Web App) generator. Gatsby loads only the critical HTML, CSS, data, and JavaScript so your site loads as fast as possible. 
+                        Support for Dark Mode in iOS 13 was added. If you have iOS 13.0+, you will definetly want this update!
                       </p>
                     </a>
                   </div>
-                  <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-                    <div className="flex items-center justify-start">
-                      <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg">Action</button>
-                    </div>
-                  </div>
                 </div>
                 
-                
-                
-                <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+                <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink mb-4 mt-4">
                   <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
                     <a href="https://tailwindcss.com/" className="flex flex-wrap no-underline hover:no-underline">
-                      <p className="w-full text-gray-600 text-xs md:text-sm px-6">Tailwind is different</p>
-                      <div className="w-full font-bold text-xl text-gray-800 px-6">State-of-the-art CSS Framework</div>
+                      <p className="w-full text-gray-600 text-xs md:text-sm px-6">Trip Stops</p>
+                      <div className="w-full font-bold text-xl text-gray-800 px-6">Version 2.0</div>
                       <p className="text-gray-800 text-base px-6 mb-5">
-                        Instead of opinionated predesigned components, Tailwind provides low-level utility classes that let you build completely custom designs without ever leaving your HTML.
+                        You are now able to select the places you are planning on stopping at, and select a time in which you plan on leaving.
+                      </p>
+                      <p className="text-gray-800 text-base px-6 mb-5">
+                        Redesign of the trip time and distance label so it not only looks better, but also tells you more information.
+                      </p>
+                      <p className="text-gray-800 text-base px-6 mb-5">
+                        We also fixed some design issues that caused the app to render awkwardly on the iPhone SE.
                       </p>
                     </a>
-                  </div>
-                  <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-                    <div className="flex items-center justify-center">
-                      <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg">Action</button>
-                    </div>
                   </div>
                 </div>
-                
-                
-                
-                <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+                       
+                <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink mb-4 mt-4">
                   <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
                     <a href="#" className="flex flex-wrap no-underline hover:no-underline">
-                      <p className="w-full text-gray-600 text-xs md:text-sm px-6">Open-Source</p>
-                      <div className="w-full font-bold text-xl text-gray-800 px-6">Coded by <a href="https://twitter.com/Sm0keDev">Sm0ke</a> </div>
+                      <p className="w-full text-gray-600 text-xs md:text-sm px-6">Starting Times and Night Time</p>
+                      <div className="w-full font-bold text-xl text-gray-800 px-6">Version 1.2</div>
                       <p className="text-gray-800 text-base px-6 mb-5">
-                        The source code is released under a license in which the copyright holder grants users the rights to study, change, and distribute the software to anyone and for any purpose.
+                        You are now able to select your own starting time, allowing you to better plan ahead.
+                      </p>
+                      <p className="text-gray-800 text-base px-6 mb-5">
+                        Night Time icons were added. If you traveling during the wee hours of the night, the new night time icons along with a darker colored cell will be shown.
                       </p>
                     </a>
-                  </div>
-                  <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-                    <div className="flex items-center justify-end">
-                      <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg">Action</button>
-                    </div>
                   </div>
                 </div>
                 
@@ -422,58 +412,33 @@ function Layout({ children }) {
                   <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
                 </div>
                 
-                <div className="flex flex-col sm:flex-row justify-center pt-12 my-12 sm:my-4">
-                  
-                  <div className="flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-white mt-4">
-                    <div className="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow">
-                      <div className="p-8 text-3xl font-bold text-center border-b-4">Free</div>
-                      <ul className="w-full text-center text-sm">
-                        <li className="border-b py-4">Thing</li>
-                        <li className="border-b py-4">Thing</li>
-                        <li className="border-b py-4">Thing</li>
-                      </ul>
-                    </div>
-                    <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-                      <div className="w-full pt-6 text-3xl text-gray-600 font-bold text-center">£0 <span className="text-base">for one user</span></div>
-                      <div className="flex items-center justify-center">
-                        <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg">Sign Up</button>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="flex flex-col w-5/6 lg:w-1/3 mx-auto lg:mx-0 rounded-lg bg-white mt-4 sm:-mt-6 shadow-lg z-10">
+                <div className="flex flex-col sm:flex-row justify-center pt-12 my-12 sm:my-4">               
+                  <div className="flex flex-col w-5/6 lg:w-1/3 mr-4 rounded-lg bg-white mt-4 sm:-mt-6 shadow-lg z-10">
                     <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-                      <div className="w-full p-8 text-3xl font-bold text-center">Basic</div>
+                      <div className="w-full p-8 text-3xl font-bold text-center">iOS App + Web</div>
                       <div className="h-1 w-full gradient my-0 py-0 rounded-t"></div>
                       <ul className="w-full text-center text-base font-bold">
-                        <li className="border-b py-4">Thing</li>
-                        <li className="border-b py-4">Thing</li>
-                        <li className="border-b py-4">Thing</li>
-                        <li className="border-b py-4">Thing</li>
+                        <li className="border-b py-4">Plan your trips from your iPhone or iPad</li>
+                        <li className="border-b py-4">Plan your trips from the Web</li>
+                        <li className="border-b py-4">Ad-free Web App Experience</li>
                       </ul>					
                     </div>
                     <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-                      <div className="w-full pt-6 text-4xl font-bold text-center">£x.99 <span className="text-base">/ per user</span></div>
-                      <div className="flex items-center justify-center">
-                        <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg">Sign Up</button>
-                      </div>
+                      <div className="w-full pt-6 text-4xl font-bold text-center">$1.99</div>
                     </div>
                   </div>
                   
-                  <div className="flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-white mt-4">
-                    <div className="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow">
-                      <div className="p-8 text-3xl font-bold text-center border-b-4">Pro</div>
-                      <ul className="w-full text-center text-sm">
-                        <li className="border-b py-4">Thing</li>
-                        <li className="border-b py-4">Thing</li>
-                        <li className="border-b py-4">Thing</li>
+                  <div className="flex flex-col w-5/6 lg:w-1/3 rounded-lg ml-4 bg-white mt-4 sm:-mt-6 shadow-lg z-10">
+                    <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+                      <div className="w-full p-8 text-3xl font-bold text-center">Web</div>
+                      <div className="h-1 w-full gradient my-0 py-0 rounded-t"></div>
+                      <ul className="w-full text-center text-base font-bold">
+                        <li className="border-b py-4">Plan your trips from the Web</li>
+                        <li className="border-b py-4">Ads will be shown</li>
                       </ul>
                     </div>
                     <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-                      <div className="w-full pt-6 text-3xl text-gray-600 font-bold text-center">£x.99 <span className="text-base">/ per user</span></div>
-                      <div className="flex items-center justify-center">
-                        <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg">Sign Up</button>
-                      </div>
+                      <div className="w-full pt-6 text-4xl font-bold text-center">Free</div>
                     </div>
                   </div>
 
