18eac948762ad57cdc5b1a9392b3ea8fffafcc08
Josh Sauder <joshsauder@gmail.com>
2020-01-16 23:08:13 -0500

    updated logo

--- src/components/layout.js
+++ src/components/layout.js
@@ -125,6 +125,7 @@ function Layout({ children }) {
 
                 </div>
 
+
                 <div className="w-full md:w-3/5 py-6 pb-10 text-center">
                   <Img className="w-full md:w-4/5 z-50" sizes={data.file.childImageSharp.sizes} alt="Logo" />
                 </div>
